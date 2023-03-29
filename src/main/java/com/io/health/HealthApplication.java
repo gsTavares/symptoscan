851420c7e67cabdcca6fe860702b32e5711b43e7
gsTavares <gustavo.tavares24@outlook.com>
2023-03-29 19:25:45 -0300

    feat: docs with OpenAPI

--- src/main/java/com/io/health/HealthApplication.java
+++ src/main/java/com/io/health/HealthApplication.java
@@ -3,7 +3,16 @@ package com.io.health;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
+
 @SpringBootApplication
+@SecurityScheme(type = SecuritySchemeType.HTTP, name = "Authorization", scheme = "bearer")
+@OpenAPIDefinition(security = @SecurityRequirement(name = "bearer-key"), info = @Info(title = "HealthIO REST API v1.0", contact = @Contact(name = "Gustavo Tavares", url = "https://github.com/gsTavares", email = "gustavo.silva365@fatec.sp.gov.br")))
 public class HealthApplication {
 
 	public static void main(String[] args) {
