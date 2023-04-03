dca4f1aa1bc33b3dcd7c2be5bb45bd6e482d3785
Gustavo Tavares <gustavo.tavares24@outlook.com>
2023-04-02 21:43:31 -0300

    fix: API security scheme definition

--- src/main/java/com/io/health/HealthApplication.java
+++ src/main/java/com/io/health/HealthApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeIn;
 import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
 import io.swagger.v3.oas.annotations.info.Contact;
 import io.swagger.v3.oas.annotations.info.Info;
@@ -11,8 +12,8 @@ import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import io.swagger.v3.oas.annotations.security.SecurityScheme;
 
 @SpringBootApplication
-@SecurityScheme(type = SecuritySchemeType.HTTP, name = "Authorization", scheme = "bearer")
-@OpenAPIDefinition(security = @SecurityRequirement(name = "bearer-key"), info = @Info(title = "HealthIO REST API v1.0", contact = @Contact(name = "Gustavo Tavares", url = "https://github.com/gsTavares", email = "gustavo.silva365@fatec.sp.gov.br")))
+@SecurityScheme(type = SecuritySchemeType.HTTP, name = "Bearer Authentication", scheme = "bearer", bearerFormat = "JWT", in = SecuritySchemeIn.HEADER)
+@OpenAPIDefinition(security = @SecurityRequirement(name = "Bearer Authentication"), info = @Info(title = "HealthIO REST API v1.0", contact = @Contact(name = "Gustavo Tavares", url = "https://github.com/gsTavares", email = "gustavo.silva365@fatec.sp.gov.br")))
 public class HealthApplication {
 
 	public static void main(String[] args) {
